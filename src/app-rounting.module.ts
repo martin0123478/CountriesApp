f2dfdd679f99d1485022e0406388c5748f2f39e5
martin0123478 <martin_agui97@outlook.es>
2021-07-29 17:52:34 -0500

    se crea la estructura principal del proyecto dividiendolo en modulos y componentes

---
+++ src/app-rounting.module.ts
@@ -0,0 +1,37 @@
+import{NgModule} from '@angular/core';
+import { Routes } from '@angular/router';
+import { PorPaisComponent } from './app/pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './app/pais/pages/por-region/por-region.component';
+import { PorCapitalComponent } from './app/pais/pages/por-capital/por-capital.component';
+import { VerPaisComponent } from './app/pais/pages/ver-pais/ver-pais.component';
+
+const routes:Routes = [
+  {
+    path:'',
+    component:PorPaisComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'region',
+    component:PorRegionComponent
+  },
+  {
+    path:'capital',
+    component:PorCapitalComponent
+  },
+  {
+    path:'pais/:id',
+    component:VerPaisComponent
+  }
+]
+@NgModule({
+  imports:[
+
+  ],
+  exports:[
+
+  ]
+})
+export class AppRoutitngModule{
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    PaisModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
