c9f0eae2904095afa7201ec675edbeccf8e89089
martin0123478 <martin_agui97@outlook.es>
2021-08-02 22:42:10 -0500

    se manejaron los errores en la busqueda

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -8,16 +8,21 @@ import { PaisService } from '../../services/pais.service';
   ]
 })
 export class PorPaisComponent implements OnInit {
-  termino:string=''
+  termino:string='';
+  hayError : boolean = false;
   constructor(private paisservice:PaisService) { }
 
   ngOnInit(): void {
   }
 
   buscar(){
+    this.hayError=false;
     this.paisservice.buscarPais(this.termino)
     .subscribe((data)=>{
+      
       console.log(data)
+    },(err)=>{
+      this.hayError = true;
     })
   }
 
